565a045a740e04744ad3dd246641786567dba8a2
BBarricklow <BLBarricklow@gmail.com>
2022-06-06 00:37:35 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+//const { request } = require('express');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8000;
+
+app.use(cors());
+
+const dbFiguarts = {
+    "piccolo": {
+        "name" : "Piccolo",
+        "number" : 1,
+        "release" : "November 2009",
+        "releasetype" : "General"
+    },
+    "supersaiyansongoku": {
+        "name" : "Super Saiyan Son Goku",
+        "number" : 2,
+        "release" : "January 2010",
+        "releasetype" : "General"
+    },
+    "supersaiyansongohan": {
+        "name" : "Super Saiyan Son Gohan",
+        "number" : 3,
+        "release" : "July 2010",
+        "releasetype" : "General"
+    },
+    "supersaiyanvegeta": { 
+        "name" : "Super Saiyan Vegeta",
+        "number" : 4,
+        "release" : "December 2011",
+        "releasetype" : "Premium Bandai"
+    },
+    "unknown": {
+        "name" : "???",
+        "number" : NaN,
+        "release" : "???",
+        "releasetype" : "???"
+    },
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/api/', (req, res) => {
+    res.json(dbFiguarts);
+})
+
+app.get('/api/:name', (req, res)=> {
+    const passname = req.params.name.toLowerCase();
+    //chect that the given request is in the api obj, then serve the nested obj
+    if (dbFiguarts[passname]) {
+        res.json(dbFiguarts[passname])
+    }
+    if (Object.keys(dbFiguarts).reduce((str, cur) => str + cur, "").includes(passname)) {
+        let returnArr = [];
+        for (let item in dbFiguarts) {
+            console.log(item)
+            if (item.includes(passname)) {
+                returnArr.push(dbFiguarts[item]);
+            }
+        }
+        res.json(returnArr);
+    }
+
+    res.json(dbFiguarts['unknown']);
+})
+//set environmental variable port or our own
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log(`The server is now running on port ${PORT}`);
+})
